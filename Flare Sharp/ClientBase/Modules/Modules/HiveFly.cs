263d049ee1b5d59b78ed92e75190cf6a1aa28099
XxkingkaidxX <37759450+XxkingkaidxX@users.noreply.github.com>
2020-06-21 00:54:18 -0400

    Add TheHive Fly
    loli neko hentai!

---
+++ Flare Sharp/ClientBase/Modules/Modules/HiveFly.cs
@@ -0,0 +1,30 @@
+﻿using Flare_Sharp.ClientBase.Categories;
+using Flare_Sharp.Memory.FlameSDK;
+
+namespace Flare_Sharp.ClientBase.Modules.Modules
+{
+    public class HiveFly : Module
+    {
+        int delayCount = 0;
+        public HiveFly() : base("Hive Fly", CategoryHandler.registry.categories[1], (char)0x07, false)
+        {
+        }
+
+        public override void onDisable()
+        {
+            base.onDisable();
+        }
+
+        public override void onTick()
+        {
+            base.onTick();
+            Minecraft.clientInstance.localPlayer.velY = 0;
+            if (delayCount >= 45)
+            {
+                Minecraft.clientInstance.localPlayer.teleport(Minecraft.clientInstance.localPlayer.X1, Minecraft.clientInstance.localPlayer.Y1 - 0.001f, Minecraft.clientInstance.localPlayer.Z1);
+                delayCount = 0;
+            }
+            delayCount++;
+        }
+    }
+}
